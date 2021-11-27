5e093f690f933b09662ebc712a5e97af78c52dac
snyt45 <snyt45@gmail.com>
2021-11-28 01:10:21 +0900

    popup用のUIファイルを生成するためのビルド環境を作る

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { r } from './scripts/utils'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  root: r('src'),
+  build: {
+    outDir: r('extension/dist'),
+    emptyOutDir: false,
+    rollupOptions: {
+      input: {
+        popup: r('src/popup/index.html'),
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    // rewrite assets to use relative path
+    {
+      name: 'assets-rewrite',
+      enforce: 'post',
+      apply: 'build',
+      transformIndexHtml(html) {
+        return html.replace(/"\/assets\//g, '"../assets/')
+      },
+    },
+  ],
   server: {
     host: true
   }
