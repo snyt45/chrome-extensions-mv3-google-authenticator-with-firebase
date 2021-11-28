98679bff4ae82bc8721afa0e4472bcaa3a846e55
snyt45 <snyt45@gmail.com>
2021-11-28 22:56:45 +0900

    Firebaseプロジェクトを作りなおしたため、OAuth2のclient_idを修正

--- src/manifest.ts
+++ src/manifest.ts
@@ -14,7 +14,7 @@ const getManifest = () => {
       'identity'
     ],
     oauth2: {
-      client_id: '1002954059338-lmat1ot7tl8tuhlepf6s1ldjuluba4db.apps.googleusercontent.com',
+      client_id: '482857465152-0he2aisvo47gl5itdp5ahkk8qpu7tio4.apps.googleusercontent.com',
       scopes: ['https://www.googleapis.com/auth/userinfo.email']
     }
   }
