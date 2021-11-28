40e72b05c6b9da1a504d44caf2584cb591d8db04
snyt45 <snyt45@gmail.com>
2021-11-28 20:40:59 +0900

    Firebaseの初期化ができることを確認

---
+++ src/popup/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID
+}
+const app = initializeApp(firebaseConfig)
+console.log(app)
