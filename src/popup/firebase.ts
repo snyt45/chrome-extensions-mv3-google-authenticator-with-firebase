704cd1a25cc083e991e1a11c315a42ad45a29556
snyt45 <snyt45@gmail.com>
2021-11-28 23:07:39 +0900

    FirebaseでGoogle認証する

--- src/popup/firebase.ts
+++ src/popup/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider, signInWithCredential } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
@@ -9,11 +10,23 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APPID
 }
 initializeApp(firebaseConfig)
+const auth = getAuth()
 
 // ChromeアプリからGoogleログインしてトークン取得
 chrome.identity.getAuthToken(
   {interactive: true},
   (token: string) => {
     console.log('token', token)
+    // Googleログイン成功時に受け取るトークンを使ってGoogleのクレデンシャル作成
+    const credential = GoogleAuthProvider.credential(null, token)
+    console.log('credential:', credential)
+    console.log('auth:', auth)
+
+    // Googleユーザーのクレデンシャルを使ってサインイン
+    signInWithCredential(auth, credential).then((result) => {
+      console.log("Sign In Success", result)
+    }).catch((error) => {
+      console.log("Sign In Error", error)
+    })
   }
 )
